f322d04a7e546e6e4a91b1dc9b1a809a887c5bcd
Xue <xuefen@merck.com>
2016-11-01 15:44:26 +0800

    2016/11/01 15:44 Prepare test Cases

--- src/com/csd/Test/HundredDoorsTest.java
+++ src/com/csd/Test/HundredDoorsTest.java
@@ -1,7 +1,28 @@
 package com.csd.Test;
 
+import org.junit.After;
+import org.junit.Before;
+import org.testng.annotations.Test;
+
 /**
  * Created by xuefen on 2016/11/1.
  */
 public class HundredDoorsTest {
+    @Before
+    public void before()
+    {
+
+    }
+
+    @Test
+    public void GIVEN_1_door_WHEN_ask_open_door_THEN_get_1()
+    {
+        
+    }
+
+    @After
+    public void After()
+    {
+
+    }
 }
