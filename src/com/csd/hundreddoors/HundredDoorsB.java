37b5684a535bdb7cc13f2da72212fb5912c3662c
Xue <xuefen@merck.com>
2016-11-01 22:39:20 +0800

    2016/11/01 23:38 Refactor algorithm B - Extract Method & Rename Variables

--- src/com/csd/hundreddoors/HundredDoorsB.java
+++ src/com/csd/hundreddoors/HundredDoorsB.java
@@ -17,24 +17,45 @@ public class HundredDoorsB extends HundredDoors {
         this.openDoors = new ArrayList();
         this.closeDoors = new ArrayList();
 
+        toggleAllDoors();
+    }
+
+    private void toggleAllDoors() {
         for(int doorNumber = 1;doorNumber<=this.NumOfDoors;doorNumber++)
         {
-            int toggleCount = 0;
-            for(int i=1;i<=doorNumber;i++)
+            toggleDoor(doorNumber);
+        }
+    }
+
+    private void toggleDoor(int doorNumber) {
+        int toggleCount = getToggleCount(doorNumber);
+        if(toggleCount%2==0)
+        {
+            closeDoor(doorNumber);
+        }else
+        {
+            openDoor(doorNumber);
+        }
+    }
+
+    private void openDoor(int doorNumber) {
+        this.openDoors.add(doorNumber);
+    }
+
+    private void closeDoor(int doorNumber) {
+        this.closeDoors.add(doorNumber);
+    }
+
+    private int getToggleCount(int doorNumber) {
+        int toggleCount = 0;
+        for(int i=1;i<=doorNumber;i++)
+        {
+            if(doorNumber%i==0)
             {
-                if(doorNumber%i==0)
-                {
-                    toggleCount++;
-                }
-            }
-            if(toggleCount%2==0)
-            {
-                this.closeDoors.add(doorNumber);
-            }else
-            {
-                this.openDoors.add(doorNumber);
+                toggleCount++;
             }
         }
+        return toggleCount;
     }
 
     public List getOpenDoors() {
