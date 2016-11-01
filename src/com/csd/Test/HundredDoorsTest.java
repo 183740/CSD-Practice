2bc9b5c27d1f76f282666cc399934ccd8988b28a
Xue <xuefen@merck.com>
2016-11-01 15:56:50 +0800

    2016/11/01 15:56 Test Case: GIVEN_1_door_WHEN_ask_open_door_THEN_get_1

--- src/com/csd/Test/HundredDoorsTest.java
+++ src/com/csd/Test/HundredDoorsTest.java
@@ -1,8 +1,11 @@
 package com.csd.Test;
 
+import com.csd.hundreddoors.HundredDoors;
 import org.junit.After;
 import org.junit.Before;
 import org.testng.annotations.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by xuefen on 2016/11/1.
@@ -17,7 +20,8 @@ public class HundredDoorsTest {
     @Test
     public void GIVEN_1_door_WHEN_ask_open_door_THEN_get_1()
     {
-        
+        HundredDoors hundredDoors = new HundredDoors(1);
+        assertEquals(1,hundredDoors.getOpenDoors());
     }
 
     @After
---
+++ src/com/csd/hundreddoors/HundredDoors.java
@@ -0,0 +1,17 @@
+package com.csd.hundreddoors;
+
+/**
+ * Created by xuefen on 2016/11/1.
+ */
+public class HundredDoors {
+    private int length;
+
+    public HundredDoors(int length)
+    {
+        this.length = length;
+    }
+
+    public int getOpenDoors() {
+        return 1;
+    }
+}
