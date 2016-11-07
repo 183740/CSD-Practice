7e2327c63c3c108b8c1c41e706c5f87cb2fadbfe
Xue <xuefen@merck.com>
2016-11-07 23:33:56 +0800

    2016/11/7 23:33 Create Test Case: GIVEN_Roman_WHEN_give_3_THEN_return_III

--- src/com/csd/RomanNumerals.java
+++ src/com/csd/RomanNumerals.java
@@ -5,6 +5,10 @@ package com.csd;
  */
 public class RomanNumerals {
     public String generate(int i) {
-        return "I";
+        if(i==1)
+            return "I";
+        else if (i==2)
+            return "II";
+        return "III";
     }
 }
--- src/com/csd/RomanNumeralsTest.java
+++ src/com/csd/RomanNumeralsTest.java
@@ -8,10 +8,21 @@ import static org.junit.Assert.assertEquals;
  * Created by xuefen on 2016/11/7.
  */
 public class RomanNumeralsTest {
-    @Test
-    public void GIVEN_Roman_WHEN_give_1_THEN_return_I()
+    @Test public void GIVEN_Roman_WHEN_give_1_THEN_return_I()
     {
         RomanNumerals romanNumerals = new RomanNumerals();
         assertEquals("I",romanNumerals.generate(1));
+    }
+
+    @Test public void GIVEN_Roman_WHEN_give_2_THEN_return_II()
+    {
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("II",romanNumerals.generate(2));
+    }
+
+    @Test public void GIVEN_Roman_WHEN_give_3_THEN_return_III()
+    {
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("III",romanNumerals.generate(3));
     }
 }
