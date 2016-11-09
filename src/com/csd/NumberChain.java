3b1b7fc2045c036527cf4d12d307bdeb285740c8
Xue <xuefen@merck.com>
2016-11-09 20:54:31 +0800

    2016/11/09 20:53 Test Case: GIVEN_String_123_WHEN_Sorting_THEN_get_321

--- src/com/csd/NumberChain.java
+++ src/com/csd/NumberChain.java
@@ -5,7 +5,62 @@ package com.csd;
  */
 public class NumberChain {
 
-    public String getReverse(String s) {
-        return "21";
+    public String getDescending(String s)
+    {
+        int[] originalNumber = new int[s.length()];
+        for(int i=0;i<s.length();i++)
+        {
+            originalNumber[i] = Integer.parseInt(s.substring(i,i+1));
+        }
+
+        int temp = 0;
+        for (int i = originalNumber.length - 1; i > 0; --i)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                if (originalNumber[j + 1] > originalNumber[j])
+                {
+                    temp = originalNumber[j];
+                    originalNumber[j] = originalNumber[j + 1];
+                    originalNumber[j + 1] = temp;
+                }
+            }
+        }
+
+        String descendingString = "";
+        for(int i=0;i<originalNumber.length;i++)
+        {
+            descendingString+= originalNumber[i];
+        }
+        return descendingString;
+    }
+
+    public String getAscending(String s) {
+        int[] originalNumber = new int[s.length()];
+        for(int i=0;i<s.length();i++)
+        {
+            originalNumber[i] = Integer.parseInt(s.substring(i,i+1));
+        }
+
+        int temp = 0;
+        for (int i = originalNumber.length - 1; i > 0; --i)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                if (originalNumber[j + 1] < originalNumber[j])
+                {
+                    temp = originalNumber[j];
+                    originalNumber[j] = originalNumber[j + 1];
+                    originalNumber[j + 1] = temp;
+                }
+            }
+        }
+
+        String aescendingString = "";
+        for(int i=0;i<originalNumber.length;i++)
+        {
+            aescendingString+= originalNumber[i];
+        }
+        return aescendingString;
     }
 }
--- src/com/csd/NumberChainTest.java
+++ src/com/csd/NumberChainTest.java
@@ -9,10 +9,17 @@ import static org.junit.Assert.assertEquals;
  */
 public class NumberChainTest {
 
-    @Test public void GIVEN_String_12_WHEN_Reserse_THEN_get_21()
+    @Test public void GIVEN_String_12_WHEN_Descende_THEN_get_21()
     {
         NumberChain numberChain = new NumberChain();
-        assertEquals("21", numberChain.getReverse("12"));
+        assertEquals("21", numberChain.getDescending("12"));
+        assertEquals("12", numberChain.getAscending("21"));
+    }
 
+    @Test public void GIVEN_String_123_WHEN_Descende_THEN_get_321()
+    {
+        NumberChain numberChain = new NumberChain();
+        assertEquals("321", numberChain.getDescending("123"));
+        assertEquals("123", numberChain.getAscending("321"));
     }
 }
