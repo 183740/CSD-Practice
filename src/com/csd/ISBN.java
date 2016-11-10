09e8dd8c3bc881128890df19c289a4548583a26f
Xue <xuefen@merck.com>
2016-11-10 23:50:47 +0800

    2016/11/10 23:50 Prepare Project Environment

---
+++ src/com/csd/ISBN.java
@@ -0,0 +1,7 @@
+package com.csd;
+
+/**
+ * Created by xuefen on 2016/11/10.
+ */
+public class ISBN {
+}
---
+++ src/com/csd/ISBNTest.java
@@ -0,0 +1,14 @@
+package com.csd;
+
+import org.junit.Test;
+
+/**
+ * Created by xuefen on 2016/11/10.
+ */
+public class ISBNTest {
+    @Test public void GIVEN_ISBN_WHEN_Validate_THEN_GET_true()
+    {
+        ISBN isbn = new ISBN();
+
+    }
+}
